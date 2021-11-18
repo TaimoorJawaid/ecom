8c23e4c4a5a93ca5370e61150ca6d942e0c69420
TaimoorJawaid <63250091+TaimoorJawaid@users.noreply.github.com>
2021-11-18 19:27:21 +0500

    make changes to login form also added db and entries

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,9 +2,9 @@
 @section("content")
 <div class="container">
     <div class="row">
-        <div class="col-sm-4 col-sm-offset-4">
+        <div class="col-sm-4 mt-5" style="margin: 0 auto">
             <form>
-                <div class="mb-3">
+                <div class="mb-3 mt-5">
                     <label for="exampleInputEmail1" class="form-label font-weight-bold">Email address</label>
                     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                     <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ Route::get('/', function () {
     return view('login');
 });
 
-Auth::routes();
+
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
