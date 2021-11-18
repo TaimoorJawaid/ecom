f6725b5a5f6d986746bb8e3dfcc82ddea39090bc
TaimoorJawaid <63250091+TaimoorJawaid@users.noreply.github.com>
2021-11-18 21:14:57 +0500

    created session and login with database also created some controllers and middleware for session login added some changes in kernal.php added route in web.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,21 +3,19 @@
 <div class="container">
     <div class="row">
         <div class="col-sm-4 mt-5" style="margin: 0 auto">
-            <form>
+            <form action = "login" method="POST">
                 <div class="mb-3 mt-5">
+                    @csrf
                     <label for="exampleInputEmail1" class="form-label font-weight-bold">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    <input type="email" name = "emailText" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                     <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
                 </div>
                 <div class="mb-3">
                     <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1">
+                    <input type="password" name="passwordText" class="form-control" >
                 </div>
-                <div class="mb-3 form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                
+                <button type="submit" class="btn btn-primary">Login</button>
             </form>
         </div>
     </div>
